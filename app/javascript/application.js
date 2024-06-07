902cd90e758b8de3c481a98536d0d7064d57f339
Christian Sutter <git@doublesignal.com>
2024-06-07 15:04:45 +0000

    Require all JS and CSS for simplicity

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
 //= require govuk_publishing_components/dependencies
-//= require govuk_publishing_components/lib
+//= require govuk_publishing_components/all_components
