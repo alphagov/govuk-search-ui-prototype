a4c880bc4efddf15705dc35dce9ecfb88dc39b19
Christian Sutter <git@doublesignal.com>
2024-06-10 14:34:28 +0000

    Add basic complete query support

--- app/lib/discovery_engine.rb
+++ app/lib/discovery_engine.rb
@@ -4,20 +4,35 @@ module DiscoveryEngine
   extend self
 
   def search(query)
-    client.search(
+    search_service.search(
       query:,
       page_size: PAGE_SIZE,
       serving_config:,
     ).response
   end
 
+  def complete(query)
+    completion_service.complete_query(
+      query:,
+      data_store:,
+    )
+  end
+
 private
 
-  def client
-    @client ||= ::Google::Cloud::DiscoveryEngine.search_service(version: :v1)
+  def search_service
+    @search_service ||= ::Google::Cloud::DiscoveryEngine.search_service(version: :v1)
+  end
+
+  def completion_service
+    @completion_service ||= ::Google::Cloud::DiscoveryEngine.completion_service(version: :v1)
   end
 
   def serving_config
     ENV.fetch("DISCOVERY_ENGINE_SERVING_CONFIG")
+  end
+
+  def data_store
+    ENV.fetch("DISCOVERY_ENGINE_DATASTORE")
   end
 end
