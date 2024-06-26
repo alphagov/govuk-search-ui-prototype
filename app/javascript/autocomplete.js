fec4a1e93ad381b0a431c6dd30d18415c15545ab
alex.bowen <alex.bowen@digital.cabinet-office.gov.uk>
2024-06-26 15:37:43 +0100

    autocomplete styling and custom template for results

--- app/javascript/autocomplete.js
+++ app/javascript/autocomplete.js
@@ -10,14 +10,25 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
 
   Modules.AccessibleAutocomplete = function () {
 
+    var currentInputValue = '';
+    var formInput = document.querySelector('.gem-c-search__input');
+
     var source = async function (query) {
       const response = await fetch(`/api/autocompletes?q=${query}`);
       var completes = await response.json();
+      currentInputValue = query
       return completes
     }
 
+    var renderResultHtml = function (result, props,inputVal ) {
+      var index = result.toLowerCase().indexOf(inputVal.toLowerCase());
+      return `<li ${props}>${result.substring(index, index + inputVal.length)}<span class='highlight'>${result.substring(index + inputVal.length, result.length)}</span></li>`
+    }
+
     new accessibleAutocomplete('#autocomplete', {
-      search: source
+      search: source,
+      debounceTime: 100,
+      renderResult: (value, props) => renderResultHtml(value, props, currentInputValue)
     });
   }
 })(window.GOVUK.Modules)
