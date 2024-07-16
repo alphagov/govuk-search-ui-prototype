eaaa3aa44022fbb7d372187daa20f718ddba0831
alex.bowen <alex.bowen@digital.cabinet-office.gov.uk>
2024-07-16 11:54:05 +0100

    h1 responsive

--- app/javascript/autocomplete.js
+++ app/javascript/autocomplete.js
@@ -32,7 +32,6 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
     var renderResultHtml = function (result, props, inputVal) {
       var index = result.toLowerCase().indexOf(inputVal.toLowerCase());
       return `<li ${props}>${result.substring(0, index + inputVal.length)}<span class='govuk-!-font-weight-bold'>${result.substring(index + inputVal.length, result.length)}<span><div class="border">&nbsp;</div></li>`
-
     }
 
     //add google search box style behaviour to keyboard interaction
