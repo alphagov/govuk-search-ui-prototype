02ff051a45acb5a75e51d5f38b328a9c455998ef
alex.bowen <alex.bowen@digital.cabinet-office.gov.uk>
2024-07-16 11:33:50 +0100

    ac results bold

--- app/javascript/autocomplete.js
+++ app/javascript/autocomplete.js
@@ -29,14 +29,10 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
       }
     }
 
-    var renderResultHtml = function (result, props,inputVal ) {
+    var renderResultHtml = function (result, props, inputVal) {
       var index = result.toLowerCase().indexOf(inputVal.toLowerCase());
-      if (index === 0) {
-        console.log(`<li ${props}>${result.substring(index, index + inputVal.length)}<span class='govuk-!-font-weight-bold'>${result.substring(index + inputVal.length, result.length)}<span><div class="border">&nbsp;</div></li>`)
-        return `<li ${props}>${result.substring(index, index + inputVal.length)}<span class='govuk-!-font-weight-bold'>${result.substring(index + inputVal.length, result.length)}<span><div class="border">&nbsp;</div></li>`
-      } else {
-        return `<li ${props}>${result}<div class="border">&nbsp;</div></li>`
-      }
+      return `<li ${props}>${result.substring(0, index + inputVal.length)}<span class='govuk-!-font-weight-bold'>${result.substring(index + inputVal.length, result.length)}<span><div class="border">&nbsp;</div></li>`
+
     }
 
     //add google search box style behaviour to keyboard interaction
