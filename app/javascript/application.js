44eff1559da4fac76cd0e7cc40fd962ff166e5d6
alex.bowen <alex.bowen@digital.cabinet-office.gov.uk>
2024-07-16 11:10:55 +0100

    autocomplete design tweaks pt4

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,3 @@
 import './autocomplete'
 
-document.getElementById('super-search-menu-toggle').insertAdjacentHTML('beforeEnd', '<span class="navigation-search-item-hack">Search</span>');
+// document.getElementById('super-search-menu-toggle').insertAdjacentHTML('beforeEnd', '<span class="navigation-search-item-hack">Search</span>');
