8f2a03b7be75db577cd2b8be16a0592dd1a88524
Christian Sutter <git@doublesignal.com>
2024-07-23 10:03:03 +0000

    Remove controller helpers now they are in helper

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,36 +2,11 @@ require "net/http"
 require "json"
 
 class SearchesController < ApplicationController
-  before_action :level_one_topics, only: [:show]
-
-  before_action :level_two_topics, only: [:show]
-
   def show; end
 
-  helper_method :search, :query, :level_one_topics, :level_two_topics
+  helper_method :search, :query
 
 private
-
-  def level_one_topics
-    uri = URI("https://www.gov.uk/api/content")
-    response = Net::HTTP.get(uri)
-
-    @level_one_topics ||= JSON.parse(response)["links"]["level_one_taxons"].sort_by { |obj| obj[:title] }
-  end
-
-  def level_two_topics
-    if params[:level_one_taxon].present?
-
-      level_one = @level_one_topics.find { |t| t["content_id"] == params[:level_one_taxon] }
-      uri = URI("https://www.gov.uk/api/content/#{level_one['base_path']}")
-      response = Net::HTTP.get(uri)
-
-      @level_two_topics ||= JSON.parse(response)["links"]["child_taxons"].sort_by { |obj| obj[:title] }
-
-    else
-      @level_two_topics = []
-    end
-  end
 
   def search
     @search ||= DiscoveryEngine.search(query, filter_expression, params[:sort]).then { Search.new(_1) }
