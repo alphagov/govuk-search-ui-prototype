d9d86fbd20d8317c9ec8eb8a69e3826d09e823c5
alex.bowen <alex.bowen@digital.cabinet-office.gov.uk>
2024-07-24 13:07:14 +0100

    remove filter/sort autosubmit

--- app/javascript/filters.js
+++ app/javascript/filters.js
@@ -5,11 +5,11 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
   'use strict'
 
   Modules.Filters = function ($element) {
-    Array.from($element.querySelectorAll('.js-filter-autosubmit input')).forEach(function(el) {
-        el.addEventListener("change", function(e) {
-            el.closest("form").submit();
-        });
-    });
+    // Array.from($element.querySelectorAll('.js-filter-autosubmit input')).forEach(function(el) {
+    //     el.addEventListener("change", function(e) {
+    //         el.closest("form").submit();
+    //     });
+    // });
 
     //cant seem to be able to add js-filter-autosubmit class to radio component
     // Array.from($element.querySelectorAll('input[type=radio]')).forEach(function(el) {
