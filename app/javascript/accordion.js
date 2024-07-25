2a7ba837026d0f8177fc300152aa32d02c7040d6
alex.bowen <alex.bowen@digital.cabinet-office.gov.uk>
2024-07-25 15:48:36 +0100

    remove persistence of filter panel open close and decide on state if any filters applied

--- app/javascript/accordion.js
+++ app/javascript/accordion.js
@@ -8,28 +8,16 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
 
     class Accordion {
         constructor(domNode) {
-          this.rootEl = domNode;
-          let open = this.rootEl.getAttribute('data-accordion-open');
-          this.persist = this.rootEl.getAttribute('data-accordion-persist-state');
-          this.buttonEl = this.rootEl.querySelector('button[aria-expanded]');
+            this.rootEl = domNode;
+            this.buttonEl = this.rootEl.querySelector('button[aria-expanded]');
 
-          if (this.persist) {
-            open = localStorage.getItem(this.buttonEl.id) || open;
-          }
-      
-          const controlsId = this.buttonEl.getAttribute('aria-controls');
-          this.contentEl = document.getElementById(controlsId);
+            const controlsId = this.buttonEl.getAttribute('aria-controls');
+            this.contentEl = document.getElementById(controlsId);
 
-          if (open === "false") {
-            this.contentEl.setAttribute('hidden', '');
-          } else {
-            this.contentEl.removeAttribute('hidden');
-          }
+            this.open = this.buttonEl.getAttribute('aria-expanded') === 'true';
 
-          this.buttonEl.setAttribute('aria-expanded', `${open}`);
-          this.open = this.buttonEl.getAttribute('aria-expanded') === 'true';
-      
-          this.buttonEl.addEventListener('click', this.onButtonClick.bind(this));
+            // add event listeners
+            this.buttonEl.addEventListener('click', this.onButtonClick.bind(this));
         }
       
         onButtonClick() {
@@ -46,10 +34,8 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
           this.buttonEl.setAttribute('aria-expanded', `${open}`);
           if (open) {
             this.contentEl.removeAttribute('hidden');
-            localStorage.setItem(this.buttonEl.id, true);
           } else {
             this.contentEl.setAttribute('hidden', '');
-            localStorage.setItem(this.buttonEl.id, false);
           }
         }
       
